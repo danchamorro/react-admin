01c622fbc9f4017bce56dbeffd9f261058c94d8e
danchamorro <dan@danchamorro.com>
2018-12-30 13:33:17 -0500

    Adding Creation and Editing Capabilities

--- src/posts.js
+++ src/posts.js
@@ -1,10 +1,18 @@
 import React from "react";
 import {
+  Edit,
+  SimpleForm,
+  DisabledInput,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+  LongTextInput,
   List,
   Datagrid,
   TextField,
   ReferenceField,
-  EditButton
+  EditButton,
+  Create
 } from "react-admin";
 
 export const PostList = props => (
@@ -14,10 +22,33 @@ export const PostList = props => (
       <ReferenceField source="userId" reference="users">
         <TextField source="name" />
       </ReferenceField>
-
       <TextField source="title" />
-
       <EditButton />
     </Datagrid>
   </List>
+);
+
+export const PostEdit = props => (
+  <Edit {...props}>
+    <SimpleForm>
+      <DisabledInput source="id" />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <LongTextInput source="body" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const PostCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <LongTextInput source="body" />
+    </SimpleForm>
+  </Create>
 );
